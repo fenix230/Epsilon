ca502a19aef56fbb816146c78b5a874e61c42322
fenix230 <fenix230@126.com>
2017-02-25 11:51:36 +0800

    create solution

---
+++ EpsilonEngine/Src/EpsilonEngine.cpp
@@ -0,0 +1,15 @@
+// EpsilonEngine.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "AllInOne.h"
+
+
+int main()
+{
+	epsilon::Window win("Test", 700, 300);
+	win.Run();
+
+    return 0;
+}
+
